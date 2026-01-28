190d85da39f9fefc1dce7496e7a133ccd6a372d0
Stavtsev Vladislav <147432896+statosh@users.noreply.github.com>
2026-01-29 00:39:13 +0300

    Add files via upload

--- src/test/java/com/example/SecondTask/service/TaskService.java
+++ src/test/java/com/example/SecondTask/service/TaskService.java
@@ -1,4 +1,45 @@
 package com.example.SecondTask.service;
 
+import com.example.SecondTask.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.SecondTask.repository.TaskRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(UUID id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(UUID id, Task updatedTask) {
+        if (!taskRepository.findById(id).isPresent()) {
+            return null;
+        }
+        updatedTask.setId(id);
+        return taskRepository.save(updatedTask);
+    }
+
+    public boolean deleteTask(UUID id) {
+        return taskRepository.deleteById(id);
+    }
 }
