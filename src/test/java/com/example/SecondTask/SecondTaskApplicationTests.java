61560a3b9842efc41228f4ac17733d128d826bc0
Stavtsev Vladislav <147432896+statosh@users.noreply.github.com>
2026-01-29 00:24:27 +0300

    Add files via upload

---
+++ src/test/java/com/example/SecondTask/SecondTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
